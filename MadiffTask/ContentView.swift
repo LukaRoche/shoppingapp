d75fb31cb1152da4577d9e116b45f3f13c4c99e1
Luka <163368776+LukaRoche@users.noreply.github.com>
2024-04-02 09:54:05 +0200

    Initial Commit

---
+++ MadiffTask/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MadiffTask
+//
+//  Created by Łukasz Stoiński on 02/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MadiffTask/MadiffTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  MadiffTaskApp.swift
+//  MadiffTask
+//
+//  Created by Łukasz Stoiński on 02/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct MadiffTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
